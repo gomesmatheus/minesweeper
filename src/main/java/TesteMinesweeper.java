1d998dfa8b80134a25b782b78c04b7fd69703dc0
gomesmatheus <42104666+gomesmatheus@users.noreply.github.com>
2019-11-02 00:40:03 -0300

    Otimização marcação

---
+++ src/main/java/TesteMinesweeper.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+public class TesteMinesweeper {
+
+	@Test
+	public void testeZap() throws InterruptedException {
+		/* trocar path para local onde o chromedriver está */
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\NAJA INFORMATICA\\Downloads\\a\\chromedriver.exe");		
+		Tabuleiro tabuleiro = Tabuleiro.getInstance();
+//		tabuleiro.trocaDificuldade();
+//		tabuleiro.trocaDificuldade();
+		tabuleiro.trocaDificuldade();
+		tabuleiro.inicializaMatriz(16);
+//		tabuleiro.inicializaMatriz(9);
+//		tabuleiro.inicializaMatriz(50);
+		tabuleiro.run();
+	}
+	
+}
+	
